a5dd974e68c75204232f0ec41a7a7de8c80a7efa
Mihaly Farkas <>
2026-08-27 17:30:55 +0200

    chore: add Copier template update script

---
+++ tool/dev/update-copier-template.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+#######################################################################################################################
+# Update Copier template
+#######################################################################################################################
+
+# Set strict mode for bash
+set -euo pipefail
+
+# Set strict mode for bash
+set -euo pipefail
+
+# Get the absolute path of the script directory and project root
+script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+project_dir="$(cd "${script_dir}/../.." && pwd)"
+
+# Change the working directory to the project root
+cd "${project_dir}" || exit 1
+
+# Load ANSI color escape sequences
+source tool/dev/bash_utils/ansi-colors.sh
+
+#######################################################################################################################
+
+echo "${MESSAGE_COLOR}Updating Copier template...${NO_COLOR}"
+set -x
+copier update --vcs-ref main
+{ set +x; } 2>/dev/null
+echo
